b3188eec45756f8a4edf28aea62dfd58d294cb5f
farinajf@gmail.com <farinajf@gmail.com>
2018-04-24 23:45:57 +0200

    third commit

--- org.wso2.carbon.esb.connector.cassoa/src/main/java/org/wso2/carbon/esb/connector/oauth/old/OAuth2Provider.java
+++
@@ -1,8 +0,0 @@
-package org.wso2.carbon.esb.connector.oauth.old;
-
-import java.io.IOException;
-import org.wso2.carbon.esb.connector.oauth.JsonParseException;
-
-public interface OAuth2Provider {
-	Object getUSerProfile(final String accessToken) throws JsonParseException, IOException;
-}
